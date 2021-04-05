8983eb5ff873bb07588984433338c21c388ad391
Kevin Mezu <kevinmezu@Kevins-Mac-mini.fios-router.home>
2021-04-05 15:51:57 -0400

    initial commit

--- fut-players/app.js
+++ fut-players/app.js
@@ -17,6 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/players', playerRouter)
+app.use('/players', playerRouter);
 
 module.exports = app;
